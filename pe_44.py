54328728a762579f5682a451a327cd269c9da3bf
Steven Mathey <72931382+sethyvenatem@users.noreply.github.com>
2020-10-26 13:39:53 +0100

    Update pe_44.py
    Added a stopping condition and some comments.

--- pe_44.py
+++ pe_44.py
@@ -6,9 +6,11 @@ def M(k,r):
 	return (1+math.sqrt(1+12*(3*r**2+6*k*r-r)))/6
 
 k=1
-while k<10000:
+while (k<10000 or (abs(round(n)-n)>10**(-12) and abs(round(m)-m)>10**(-12))):
+#The loop stops either when k=100000 or when a solution is found.
 	r=1
-	while r<10000:
+	while r<(r<10000 or (abs(round(n)-n)>10**(-12) and abs(round(m)-m)>10**(-12))):
+		#The loop stops either when r=10000 or when a solution is found.
 		n=N(k,r)
 		m=M(k,r)
 		if (abs(round(n)-n)<10**(-12) and abs(round(m)-m)<10**(-12)):
@@ -17,6 +19,7 @@ while k<10000:
 	k=k+1
 	if (round(k/10)==k/10):
 		print(k/10)
+		#This displays the progress of the code. It helps to know that the computer is working as intended. Only 1 in 10 steps is displayed.
 
 #solution
 # 1020 1147 2395.0 1912.0
